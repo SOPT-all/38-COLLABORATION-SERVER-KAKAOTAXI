1c9c371b521bdb23cb5f334855b86782c4b0af58
kimhoeyun <clnvrd@suwon.ac.kr>
2026-05-09 02:44:32 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/sopt/kakaotaxi/KakaotaxiApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.kakaotaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaotaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaotaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopt/kakaotaxi/KakaotaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.kakaotaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaotaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
