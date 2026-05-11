491d208e093cd0c23d0f3de0505eccdc9cf39438
imddoy <imddoy@duksung.ac.kr>
2026-05-11 20:12:14 +0900

    5 feat: BusinessException errorcode 반영

--- src/main/java/com/sopt/kakaotaxi/global/exception/BusinessException.java
+++ src/main/java/com/sopt/kakaotaxi/global/exception/BusinessException.java
@@ -3,17 +3,22 @@ package com.sopt.kakaotaxi.global.exception;
 import java.util.Objects;
 import org.springframework.http.HttpStatus;
 
-// 현재는 HTTP 상태와 메시지만 있으나 추후 작업에서 ErrorCode 기반으로 변경 예정
+import com.sopt.kakaotaxi.global.response.code.common.ErrorCode;
+
 public class BusinessException extends RuntimeException {
 
-	private final HttpStatus status;
+	private final ErrorCode errorCode;
 
-	public BusinessException(HttpStatus status, String message) {
-		super(message);
-		this.status = Objects.requireNonNull(status);
+	public BusinessException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
+
+	public ErrorCode getErrorCode() {
+		return errorCode;
 	}
 
 	public HttpStatus getStatus() {
-		return status;
+		return HttpStatus.valueOf(errorCode.getStatus());
 	}
 }
