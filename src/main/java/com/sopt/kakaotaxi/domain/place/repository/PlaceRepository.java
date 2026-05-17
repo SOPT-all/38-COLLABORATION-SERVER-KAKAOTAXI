d688f9da873e30fbb54887c5579adc2deb60a854
imddoy <imddoy@duksung.ac.kr>
2026-05-17 19:55:16 +0900

    11 feat: 최근 장소 조회 api 구현

--- src/main/java/com/sopt/kakaotaxi/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/sopt/kakaotaxi/domain/place/repository/PlaceRepository.java
@@ -1,7 +1,11 @@
 package com.sopt.kakaotaxi.domain.place.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sopt.kakaotaxi.domain.place.entity.Place;
+import com.sopt.kakaotaxi.domain.place.entity.PlaceType;
 
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
+	List<Place> findByUserIdOrderByLastVisitedAtDesc(Long userId);
 }
